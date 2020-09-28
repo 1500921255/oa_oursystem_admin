dc28023f152dc56945be1678a7c1774835544c34
lxh <2985736421@qq.com>
2020-09-28 23:39:54 +0800

    新增了employee_user分页查询接口

---
+++ oa_systems/src/test/java/com/example/demo/service/impl/EmployeeUserServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeUserServiceImplTest {
+
+    @Test
+    void selectAll() {
+
+    }
+}
